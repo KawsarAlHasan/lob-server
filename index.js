35a55ff050f8ae6ff4b75077c4b8d1999fad4887
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2023-10-12 04:08:27 +0600

    CURD oparation for all category

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zoxi4qf.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -21,16 +19,96 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    const grandCtgCollection = client.db("lob").collection("grandcategory");
+    const parentCtgCollection = client.db("lob").collection("parentcategory");
+    const childCtgCollection = client.db("lob").collection("childcategory");
+
+    // start
+    // category start
+    // grand category start
+    app.get("/grandcategory", async (req, res) => {
+      const result = await grandCtgCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/grandcategory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await grandCtgCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/grandcategory", async (req, res) => {
+      const newGrandCategory = req.body;
+      const result = await grandCtgCollection.insertOne(newGrandCategory);
+      res.send(result);
+    });
+
+    app.delete("/grandcategory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await grandCtgCollection.deleteOne(query);
+      res.send(result);
+    });
+    // grand category end
+
+    // parent category start
+    app.get("/parentcategory", async (req, res) => {
+      const result = await parentCtgCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/parentcategory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parentCtgCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/parentcategory", async (req, res) => {
+      const newParentCategory = req.body;
+      const result = await parentCtgCollection.insertOne(newParentCategory);
+      res.send(result);
+    });
+
+    app.delete("/parentcategory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parentCtgCollection.deleteOne(query);
+      res.send(result);
+    });
+    // parent category end
+
+    // child category start
+    app.get("/childcategory", async (req, res) => {
+      const result = await childCtgCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/childcategory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await childCtgCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/childcategory", async (req, res) => {
+      const newChildCategory = req.body;
+      const result = await childCtgCollection.insertOne(newChildCategory);
+      res.send(result);
+    });
+
+    app.delete("/childcategory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await childCtgCollection.deleteOne(query);
+      res.send(result);
+    });
+    // child category end
+    // category end
+    // end
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
